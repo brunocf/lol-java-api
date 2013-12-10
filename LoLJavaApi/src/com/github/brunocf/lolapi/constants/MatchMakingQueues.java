c154392b6d6a3c4dbf6742ffc55c0747104fc534
brunocf <bruno.croys@gmail.com>
2013-12-10 20:26:56 -0200

    No Deal, first

---
+++ LoLJavaApi/src/com/github/brunocf/lolapi/constants/MatchMakingQueues.java
@@ -0,0 +1,19 @@
+package com.github.brunocf.lolapi.constants;
+
+public final class MatchMakingQueues {
+
+	public static final short NOMAL_5V5_BLIND_PICK = 2;
+	public static final short RANKED_SOLO_5V5 = 4;
+	public static final short COOP_VS_AI_5V5 = 7;
+	public static final short NORMAL_3V3 = 8;
+	public static final short NORMAL_5V5_DRAFTPICK = 14;
+	public static final short DOMINION_5V5_BLIND_PICK = 16;
+	public static final short DOMINION_5V5_DRAFT_PICK = 17;
+	public static final short DOMINION_5V5_COOP_VS_AI = 25;
+	public static final short RANKED_TEAM_3V3 = 41;
+	public static final short RANKED_TEAM_5V5 = 42;
+	public static final short TWISTED_TREELINE_COOP_VS_AI = 52;
+	public static final short ARAM = 65;
+	public static final short ARAM_COOP_VS_AI = 67;
+
+}
